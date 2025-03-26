a9f662a74a1c1f163f3a255406d9a1906cc52aef
ekaterinablog89 <ekaterinablog89@gmail.com>
2025-03-26 14:08:44 -0400

    added JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM has loaded');
+document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("phone").addEventListener("input", function (e) {
+            let value = e.target.value.replace(/\D/g, ""); // Remove non-numeric characters
+            let formattedValue = "";
+    
+            if (value.length > 0) {
+                formattedValue = value.slice(0, 3);
+            }
+            if (value.length > 3) {
+                formattedValue += "-" + value.slice(3, 6);
+            }
+            if (value.length > 6) {
+                formattedValue += "-" + value.slice(6, 10);
+            }
+    
+            e.target.value = formattedValue;
+        });
 
-});
+        document.getElementById("submit_button").addEventListener("click", function (e) {
+            alert("Your form has submitted successfully!");
+        });
+
+
+    });
+
+
+
+   
